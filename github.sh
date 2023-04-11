a0683f674ad2d8f87740b953d67c96681eaeee36
Eberson <eberson.oliveira@gmail.com>
2023-04-11 19:00:34 -0300

    branch com o código da aula do dia Tue Apr 11 19:00:33     2023

---
+++ github.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#Altere as variáveis abaixo de acordo com a sua necessidade
+#na variável repositório, prefira a url do repositório baseada em https para nao termos que configurar SSH
+export name='Eberson Oliveira'
+export email="eberson.oliveira@gmail.com"
+export repositorio="https://github.com/eberson/pami-2023.git";
+
+#Daqui pra baixo mexa somente se for realmente necessário ou se souber o que está fazendo :P
+
+export branch_name="aula-$(date -I)"
+
+#add linux, mac and windows folders to gitignore
+echo "" >> .gitignore
+echo "#ignoring windows, mac and linux folders" >> .gitignore
+echo "windows" >> .gitignore
+echo "mac" >> .gitignore
+echo "linux" >> .gitignore
+
+git init
+git remote add origin $repositorio
+git checkout -b $branch_name
+
+git config --global user.name $name
+git config --global user.email $email
+
+git add .gitignore
+git commit -m "added ignored files" .gitignore
+
+git add .
+
+git commit -am "branch com o código da aula do dia $(date)"
+
+git push origin $branch_name
